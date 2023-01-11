13aadfaa904dcea347526c2f16d13822f7e8c220
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-01-11 15:50:57 -0500

    sign up front end completed

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,6 +1,34 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function SignUp() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [email, setEmail] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                password_confirmation: passwordConfirmation,
+                email,
+            }),
+        }).then((response) => {
+            if (response.ok) {
+                response.json().then((user) => console.log(user));
+            }
+        });
+    }
+
+
+
     return (
         <>
             <nav>
@@ -13,22 +41,42 @@ function SignUp() {
             </nav>
             <h1>We're on the Sign Up Page</h1>
             <div>
-                <form>
-                    <label>
+                <form onSubmit={handleSubmit}>
+                    <label className="signup_form" htmlFor="username">
                         Username:
-                        <input type="text" value={""} onChange={""}/>
+                        <input
+                            type="text"
+                            id="username"
+                            autoComplete="off"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
                     </label>
-                    <label>
+                    <label className="signup_form" htmlFor="password">
                         Password:
-                        <input type="text" value={""} onChange={""}/>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            autoComplete="off" />
                     </label>
-                    <label>
+                    <label className="signup_form" htmlFor="password">
                         Confirm Password:
-                        <input type="text" value={""} onChange={""}/>
+                        <input
+                            type="password"
+                            id="password_confirmation"
+                            value={passwordConfirmation}
+                            onChange={(e) => setPasswordConfirmation(e.target.value)}
+                            autoComplete="off" />
                     </label>
-                    <label>
+                    <label className="signup_form" htmlFor="email">
                         Email:
-                        <input type="text" value={""} onChange={""}/>
+                        <input
+                            type="text"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            autoComplete="off" />
                     </label>
                     <button>Sign Up</button>
                 </form>
