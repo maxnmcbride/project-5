f152e9e5a44b01a074bb2e763ea79f99b06ed154
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-01-13 12:01:20 -0500

    formatted some baloney

--- client/src/Components/VideoGameCard.js
+++ client/src/Components/VideoGameCard.js
@@ -1,19 +1,22 @@
 function VideoGameCard({ gameData }) {
 
-    console.log("videogamecard:", gameData)
-
-    // developer, game_description, genre, id, image_url, multiplayer (boolean)
-    // platform, release_date, theme, title
-
     return (
         <div>
-            <img src={gameData.image_url}/>
-            <h2>Title: {gameData.title}</h2>
-            <h3>Genre: {gameData.genre}</h3>
-            <h3>Description: {gameData.game_description}</h3>
-            <h3>Multiplayer: {gameData.multiplayer}</h3>
+                <img src={gameData.image_url} />
+                <> <h3>Title:</h3> <p> {gameData.title}</p> </>
+                <> <h3>Genre:</h3> <p> {gameData.genre}</p> </>
+                <> <h3>Theme:</h3> <p> {gameData.theme}</p> </>
+                <> <h3>Description:</h3> <p> {gameData.game_description} </p> </>
+                <> <h3>Multiplayer:</h3> <p>{gameData.multiplayer ? "True" : "False" }</p> </>
+                <> <h3>Platform:</h3> <p>{gameData.platform} </p> </>
+                <> <h3>Release Date:</h3> <p> {gameData.release_date} </p> </>
         </div>
     );
 }
 
 export default VideoGameCard;
+
+
+
+// developer, game_description, genre, id, image_url, multiplayer (boolean)
+// platform, release_date, theme, title
